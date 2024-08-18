ec824877d1ea65f4dd2e905d71b4371c88a0c653
Luqmaan <n11539445@qut.edu.au>
2024-08-18 12:37:49 +1000

    Luqmaan's Branch

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome to my branch, CAB302!");
+        System.out.print("Testing for making a new branch - Luqmaan");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
