6444c17764597e208be3bfa187534db3e70cf132
Mrkane123 <btlmcculloch@gmail.com>
2024-09-03 22:47:43 +1000

    Login started

--- src/main/java/com/therealworld/fitschedule/FitScheduleApp.java
+++ src/main/java/com/therealworld/fitschedule/FitScheduleApp.java
@@ -1,6 +1,5 @@
 package com.therealworld.fitschedule;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,20 +7,16 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
-
-
-
-public class HelloApplication extends Application {
+public class FitScheduleApp extends Application {
 
     // Constants defining the window title and size
-    public static final String TITLE = "Address Book";
+    public static final String TITLE = "Login";
     public static final int WIDTH = 640;
     public static final int HEIGHT = 360;
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(FitScheduleApp.class.getResource("/com/therealworld/fitschedule/login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         stage.setTitle(TITLE);
         stage.setScene(scene);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,9 @@ module com.therealworld.fitschedule {
 
     opens com.therealworld.fitschedule to javafx.fxml;
     exports com.therealworld.fitschedule;
-    exports com.therealworld.fitschedule.controller;
-    opens com.therealworld.fitschedule.controller to javafx.fxml;
+    exports com.therealworld.fitschedule.controllers;
+    opens com.therealworld.fitschedule.controllers to javafx.fxml;
     exports com.therealworld.fitschedule.model;
     opens com.therealworld.fitschedule.model to javafx.fxml;
+
 }
