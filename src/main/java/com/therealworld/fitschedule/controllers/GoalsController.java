c190573718f1df93d0937fea4462d22b43cf7b38
Mrkane123 <btlmcculloch@gmail.com>
2024-09-13 16:04:13 +1000

    Layout for goals page made. 2 Vbox and 1 HBox

--- src/main/java/com/therealworld/fitschedule/controllers/GoalsController.java
+++ src/main/java/com/therealworld/fitschedule/controllers/GoalsController.java
@@ -12,50 +12,11 @@ import javafx.scene.control.ListView;
 
 public class GoalsController {
     @FXML
-    private ListView<Contact> contactsListView;
     private IContactDAO contactDAO;
     public GoalsController() {
         contactDAO = new MockContactDAO();
     }
 
-    /**
-     * Renders a cell in the contacts list view by setting the text to the contact's full name.
-     * @param contactListView The list view to render the cell for.
-     * @return The rendered cell.
-     */
-    private ListCell<Contact> renderCell(ListView<Contact> contactListView) {
-        return new ListCell<>() {
-            /**
-             * Updates the item in the cell by setting the text to the contact's full name.
-             * @param contact The contact to update the cell with.
-             * @param empty Whether the cell is empty.
-             */
-            @Override
-            protected void updateItem(Contact contact, boolean empty) {
-                super.updateItem(contact, empty);
-                // If the cell is empty, set the text to null, otherwise set it to the contact's full name
-                if (empty || contact == null || contact.getFullName() == null) {
-                    setText(null);
-                } else {
-                    setText(contact.getFullName());
-                }
-            }
-        };
-    }
 
-    @FXML
-    public void initialize() {
-        contactsListView.setCellFactory(this::renderCell);
-        contactsListView.getItems().addAll(contactDAO.getAllContacts());
-    }
 
-    @FXML
-    private void onDelete() {
-        // Get the selected contact from the list view
-        Contact selectedContact = contactsListView.getSelectionModel().getSelectedItem();
-        if (selectedContact != null) {
-            contactDAO.deleteContact(selectedContact);
-
-        }
-    }
 }
