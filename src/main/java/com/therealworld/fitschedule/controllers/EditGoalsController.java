dafa491633c1823e2607b656ab92b46425728588
Mrkane123 <btlmcculloch@gmail.com>
2024-09-17 16:38:01 +1000

    edit Goals

---
+++ src/main/java/com/therealworld/fitschedule/controllers/EditGoalsController.java
@@ -0,0 +1,11 @@
+package com.therealworld.fitschedule.controllers;
+
+import javafx.event.ActionEvent;
+
+public class EditGoalsController {
+    public void onSaveClick(ActionEvent actionEvent) {
+    }
+
+    public void onCancelClick(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/therealworld/fitschedule/controllers/GoalsController.java
+++ src/main/java/com/therealworld/fitschedule/controllers/GoalsController.java
@@ -1,16 +1,32 @@
 package com.therealworld.fitschedule.controllers;
 
-
-import com.therealworld.fitschedule.model.Contact;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class GoalsController {
-//    public GoalsController() {
-//        contactDAO = new MockContactDAO();
-//    }
 
+    @FXML
+    private void onEditGoalsClick(ActionEvent event) {
+        try {
+            // Load the Edit Goals FXML
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/therealworld/fitschedule/edit-goals-view.fxml"));
+            Parent root = fxmlLoader.load();
 
-
+            // Create a new stage (window) for the Edit Goals UI
+            Stage stage = new Stage();
+            stage.setTitle("Edit Goals");
+            stage.initModality(Modality.APPLICATION_MODAL); // Block interaction with other windows
+            stage.setScene(new Scene(root));
+            stage.showAndWait(); // Wait until the edit goals window is closed
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
